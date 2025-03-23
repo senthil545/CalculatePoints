2fe58d917eb874358098cadf7ff57350d39b691f
senthil545 <82305064+senthil545@users.noreply.github.com>
2025-03-23 19:44:32 +0530

    Update AllConstants.java

--- src/com/constants/AllConstants.java
+++ src/com/constants/AllConstants.java
@@ -2,10 +2,12 @@ package com.constants;
 
 public class AllConstants {
 
-	public static final String HTML_PATH = "C:\\Users\\s0a0838\\Desktop\\TestMukil\\dream11\\index.html";
-	public static final String HTML_PATH_Nava = "C:\\Users\\s0a0838\\Desktop\\TestMukil\\auction11\\index.html";
+	public static final String HTML_PATH = "/Users/s0a0838/Downloads/points/dream11/index.html";
 	public static final int NON_TOPPERS_PER_ROW = 5;
-	public static final int TOTAL_PLAYERS = 12;
-	public static final String BATSMAN_STATS = "https://stats.espncricinfo.com/ci/engine/records/averages/batting.html?id=15129;type=tournament";
-	public static final String BOWLER_STATS = "https://stats.espncricinfo.com/ci/engine/records/averages/bowling.html?id=15129;type=tournament";
+	public static final int TOTAL_PLAYERS = 14;
+	public static final int PLAYERS_COUNT = 14;
+	public static final String BATSMAN_STATS = "https://www.espncricinfo.com/records/tournament/batting-most-runs-career/indian-premier-league-2025-16622";
+	public static final String BOWLER_STATS = "https://www.espncricinfo.com/records/tournament/bowling-most-wickets-career/indian-premier-league-2025-16622";
+	public static final String PLAYERS_DELIMITER = "<span class=\"ds-text-tight-s ds-font-regular ds-text-typo-primary hover:ds-text-typo-primary-hover ds-block\">";
+	public static final String FIELDING_STATS = "https://www.espncricinfo.com/records/tournament/fielding-most-catches-career/indian-premier-league-2025-16622";
 }
